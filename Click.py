f407282f41aa075f89d7021548c2ee0dbb0cbbbc
Muhammad Aliyan <135156764+MuhammadAliyan10@users.noreply.github.com>
2024-06-16 18:07:33 +0500

    Add Code

---
+++ Click.py
@@ -0,0 +1,23 @@
+import pyautogui
+import time
+
+
+
+
+#! Git Panel (24,154)
+pyautogui.click(24,154)
+time.sleep(1)
+
+#! Commit Message(97,110)
+#! Commit (124,142)
+
+
+#! Actions (202,80)
+pyautogui.click(202,80)
+time.sleep(1)
+#! Branch (421,353)
+pyautogui.click(421,353)
+time.sleep(1)
+#! Publish Branch (579,509)
+pyautogui.click(579,509)
+time.sleep(1)
---
+++ getCoordinates.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+while True:
+    position = pyautogui.position()
+    print(position)
+    #!1183, 837
+
